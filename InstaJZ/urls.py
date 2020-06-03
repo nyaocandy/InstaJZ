a4f0ba6fa93c4a027ce232a5f2af51f5327aceb0
nyaocandy <yaonie.thu@gmail.com>
2020-06-03 01:40:26 -0500

    redo commit staic view

--- InstaJZ/urls.py
+++ InstaJZ/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from Insta.views import HelloWorld
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloworld')   
+    path('admin/', admin.site.urls),
+    path('insta/', include('Insta.urls')),
 ]
